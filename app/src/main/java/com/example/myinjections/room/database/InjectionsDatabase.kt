46a74092fc4cea34cb09bc03fd3938d87b64b77a
mrkomal <gry2@vp.pl>
2021-01-17 00:32:14 +0100

    Defining InjectionsDatabase
    Creating companion object variable and funtion to save and get an instance of created database

--- app/src/main/java/com/example/myinjections/room/database/InjectionsDatabase.kt
+++ app/src/main/java/com/example/myinjections/room/database/InjectionsDatabase.kt
@@ -1,5 +1,33 @@
 package com.example.myinjections.room.database
 
-class InjectionsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.myinjections.room.model.InjectionInfo
+import com.example.myinjections.room.model.InjectionsDao
 
+@Database(entities = [InjectionInfo::class], version = 1)
+abstract class InjectionsDatabase : RoomDatabase() {
+
+    abstract fun injectionsDao(): InjectionsDao
+
+    companion object{
+        private var databaseInstance: InjectionsDatabase ?= null
+
+        fun getInstance(context: Context): InjectionsDatabase{
+            return databaseInstance ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    InjectionsDatabase::class.java,
+                    "injection_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                databaseInstance = instance
+                instance
+            }
+        }
+    }
 }
