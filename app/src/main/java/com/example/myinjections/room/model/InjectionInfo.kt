f90f95941bcdfc15f50cbd30ae96b31768bf5235
mrkomal <gry2@vp.pl>
2021-01-17 00:21:40 +0100

    Injections table
    Defining table and data that it holds

--- app/src/main/java/com/example/myinjections/room/model/InjectionInfo.kt
+++ app/src/main/java/com/example/myinjections/room/model/InjectionInfo.kt
@@ -1,3 +1,14 @@
 package com.example.myinjections.room.model
 
-data class InjectionInfo ()
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "injections_table")
+data class InjectionInfo (
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "name") @NonNull val name: String,
+    @ColumnInfo(name = "date") val date: String,
+    @ColumnInfo(name = "dose", defaultValue = "Unknown") val dose: Double
+)
