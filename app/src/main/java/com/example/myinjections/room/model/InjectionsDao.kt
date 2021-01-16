662174d860bb0e54eecb886542f385dfced1767e
mrkomal <gry2@vp.pl>
2021-01-17 00:24:03 +0100

    Injections Dao
    Defining interface with two functions - insert and get data

--- app/src/main/java/com/example/myinjections/room/model/InjectionsDao.kt
+++ app/src/main/java/com/example/myinjections/room/model/InjectionsDao.kt
@@ -1,4 +1,16 @@
 package com.example.myinjections.room.model
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface InjectionsDao {
+    @Query("SELECT * FROM injections_table")
+    fun getAllInfo(): Flow<List<InjectionInfo>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(injectionInfo: InjectionInfo)
 }
