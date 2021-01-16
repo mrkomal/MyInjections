66f1051eb11485472030aa565e9e03826e0bf3d6
mrkomal <gry2@vp.pl>
2021-01-17 00:38:22 +0100

    Creating InjectionsRepository
    Creating repository for injections with two commands that are available in Dao so far (insert, get)

---
+++ app/src/main/java/com/example/myinjections/repository/InjectionsRepository.kt
@@ -0,0 +1,24 @@
+package com.example.myinjections.repository
+
+import android.app.Application
+import com.example.myinjections.room.database.InjectionsDatabase
+import com.example.myinjections.room.model.InjectionInfo
+import com.example.myinjections.room.model.InjectionsDao
+import kotlinx.coroutines.flow.Flow
+
+class InjectionsRepository (application: Application){
+
+    private var injectionsDAO: InjectionsDao
+
+    init {
+        val injectionDatabase = InjectionsDatabase.getInstance(application)
+        injectionsDAO = injectionDatabase.injectionsDao()
+    }
+
+    fun getAllInjectionInfo() : Flow<List<InjectionInfo>> = injectionsDAO.getAllInfo()
+
+    suspend fun insertInjectionInfo(injectionInfo: InjectionInfo){
+        injectionsDAO.insert(injectionInfo)
+    }
+
+}
