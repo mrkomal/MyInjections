1dcb0ede073c2575822af1968422c79dccaa5365
mrkomal <gry2@vp.pl>
2021-01-17 23:35:20 +0100

    Desigining second CardView
    Adding Numberpicker for year and creating the title

--- app/src/main/java/com/example/myinjections/view/ui/AddInjectionActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/AddInjectionActivity.kt
@@ -3,10 +3,14 @@ package com.example.myinjections.view.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myinjections.R
+import kotlinx.android.synthetic.main.activity_add_injection.*
 
 class AddInjectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_injection)
+
+        year_picker.minValue = 1900
+        year_picker.maxValue = 2020
     }
 }
