787bf7d5774de1b08f8fbbac3956e1a3977e8dca
mrkomal <gry2@vp.pl>
2021-01-17 01:37:55 +0100

    Adding temporary recyclerView in main activity
    Creating temporary Viewmodel and recyclerview to test databse perdormace. It will be later upgraded.

--- app/src/main/java/com/example/myinjections/MainActivity.kt
+++ app/src/main/java/com/example/myinjections/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.myinjections
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myinjections.repository.InjectionsRepository
+import com.example.myinjections.room.model.InjectionInfo
+import com.example.myinjections.view.InjectionsListAdapter
+import com.example.myinjections.viewmodel.InjectionsViewModel
+import com.example.myinjections.viewmodel.InjectionsViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var injectionsViewModel: InjectionsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ///////////////////// TEMP
+        //Injection ViewModel
+        injectionsViewModel = ViewModelProvider(
+            this,
+            InjectionsViewModelFactory(InjectionsRepository(application))
+        )
+            .get(InjectionsViewModel::class.java)
+        //RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.temporary_recyclerview)
+        // Adapter listy
+        val adapter = InjectionsListAdapter()
+        // Przypisujemy adapter do naszej listy
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //Funckja do dodawania nowych wpisow
+        injectionsViewModel.insertInjectionInfo(InjectionInfo(0,"A","2020",0.2))
+        //Observer
+        injectionsViewModel.injectionsInfo.observe(this, Observer {
+                injectionsInfos -> injectionsInfos.let {
+            adapter.submitList(it)
+        }
+        })
+        ////////////////////
     }
 }
