800464552d147549581f81bacd0780083367f41e
mrkomal <gry2@vp.pl>
2021-01-18 17:18:47 +0100

    Adding inser button action listener

--- app/src/main/java/com/example/myinjections/view/ui/AddInjectionActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/AddInjectionActivity.kt
@@ -2,18 +2,31 @@ package com.example.myinjections.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myinjections.R
 import com.google.android.material.slider.Slider
 import kotlinx.android.synthetic.main.activity_add_injection.*
 import java.util.*
 
 class AddInjectionActivity : AppCompatActivity() {
+
+    companion object{
+        //UI items tags
+        const val INSERT_BUTTON_TAG = "insert_button_pressed"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_injection)
 
+        //UI items settings
         setYearPickerValues()
         setDoseSliderLabel()
+
+        //insert button pressed
+        insert_button.setOnClickListener {
+            Log.d(INSERT_BUTTON_TAG, "Insert button was pressed.")
+        }
     }
 
     private fun setDoseSliderLabel(){
