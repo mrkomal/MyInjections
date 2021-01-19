20cfce43e033c7310f47aebc4a3d485538684b8a
mrkomal <gry2@vp.pl>
2021-01-19 13:19:26 +0100

    Defining own basic alert dialog view
    Creating layout and connecting it to the class

--- app/src/main/java/com/example/myinjections/view/dialogs/MissingNameDialogFragment.kt
+++ app/src/main/java/com/example/myinjections/view/dialogs/MissingNameDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myinjections.view.dialogs
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
@@ -10,11 +11,17 @@ import com.example.myinjections.R
 class MissingNameDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity.let{
-            AlertDialog.Builder(it)
-                .setTitle("Error")
-                .setMessage("Injection\'s name is required.")
-                .setIcon(R.drawable.sharp_error_black_36dp)
-                .create()
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater;
+
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.missing_name_alert_dialog_fragment, null))
+            builder.create()
+//            AlertDialog.Builder(it)
+//                .setTitle("Error")
+//                .setMessage("Injection\'s name is required.")
+//                .setIcon(R.drawable.sharp_error_black_36dp)
+//                .create()
         }
     }
 }
