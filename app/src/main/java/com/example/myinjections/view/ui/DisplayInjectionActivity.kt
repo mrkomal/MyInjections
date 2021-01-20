76ef65d5984f24390a4526b11421ec0cdc647ec2
mrkomal <gry2@vp.pl>
2021-01-20 19:54:54 +0100

    Adding new empty activity

---
+++ app/src/main/java/com/example/myinjections/view/ui/DisplayInjectionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myinjections.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myinjections.R
+
+class DisplayInjectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_injection)
+    }
+}
