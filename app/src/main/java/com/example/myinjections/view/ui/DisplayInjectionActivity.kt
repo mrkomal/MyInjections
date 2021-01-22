f9cf5a17e2a626eef3bcc70d0a8ab5293cf78fcb
mrkomal <gry2@vp.pl>
2021-01-22 23:17:24 +0100

    Defining activity for displaying injections data.
    Recyclerview for injections data.

--- app/src/main/java/com/example/myinjections/view/ui/DisplayInjectionActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/DisplayInjectionActivity.kt
@@ -2,11 +2,32 @@ package com.example.myinjections.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myinjections.R
+import com.example.myinjections.view.adapters.InjectionsListAdapter
+import com.example.myinjections.viewmodel.BaseActivity
 
-class DisplayInjectionActivity : AppCompatActivity() {
+class DisplayInjectionActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_injection)
+
+        // RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.injections_recyclerview)
+
+        // List Adapter
+        val adapter = InjectionsListAdapter()
+
+        // Binding list adapter with RV
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Creating observer for InjectionsViewModel data
+        injectionsViewModel.injectionsInfo.observe(this, Observer { injectionsInfos ->
+            injectionsInfos.let { adapter.submitList(it) }
+        })
+
     }
 }
