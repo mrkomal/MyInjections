21921369f31a54e422e43caf737dc497fc93269e
mrkomal <gry2@vp.pl>
2021-01-22 22:33:53 +0100

    Adding injections repository implementation
    Changing parameters from context to dao (DI)

--- app/src/main/java/com/example/myinjections/repository/InjectionsRepositoryImpl.kt
+++ app/src/main/java/com/example/myinjections/repository/InjectionsRepositoryImpl.kt
@@ -1,19 +1,10 @@
 package com.example.myinjections.repository
 
-import android.app.Application
-import com.example.myinjections.room.database.InjectionsDatabase
 import com.example.myinjections.room.model.InjectionInfo
 import com.example.myinjections.room.model.InjectionsDao
 import kotlinx.coroutines.flow.Flow
 
-class InjectionsRepositoryImpl(application: Application): InjectionsRepository {
-
-    private var injectionsDAO: InjectionsDao
-
-    init {
-        val injectionDatabase = InjectionsDatabase.getInstance(application)
-        injectionsDAO = injectionDatabase.injectionsDao()
-    }
+class InjectionsRepositoryImpl(private var injectionsDAO: InjectionsDao): InjectionsRepository {
 
     override fun getAllInjectionInfo() : Flow<List<InjectionInfo>> = injectionsDAO.getAllInfo()
 
