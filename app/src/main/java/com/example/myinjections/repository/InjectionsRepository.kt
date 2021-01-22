ca4aa07e069a1de850d3d78a870b0b48a7169b6a
mrkomal <gry2@vp.pl>
2021-01-22 22:32:25 +0100

    Changing repostory interface name

--- app/src/main/java/com/example/myinjections/repository/InjectionsRepository.kt
+++ app/src/main/java/com/example/myinjections/repository/InjectionsRepository.kt
@@ -3,7 +3,7 @@ package com.example.myinjections.repository
 import com.example.myinjections.room.model.InjectionInfo
 import kotlinx.coroutines.flow.Flow
 
-interface IInjectionsRepository {
+interface InjectionsRepository {
 
     fun getAllInjectionInfo() : Flow<List<InjectionInfo>>
 
