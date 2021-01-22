6e9745c57904bf1febd1df07b38bbc4c6a88c271
mrkomal <gry2@vp.pl>
2021-01-22 22:34:42 +0100

    Removing redundant companion object

--- app/src/main/java/com/example/myinjections/room/database/InjectionsDatabase.kt
+++ app/src/main/java/com/example/myinjections/room/database/InjectionsDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.myinjections.room.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.myinjections.room.model.InjectionInfo
 import com.example.myinjections.room.model.InjectionsDao
@@ -12,22 +10,4 @@ abstract class InjectionsDatabase : RoomDatabase() {
 
     abstract fun injectionsDao(): InjectionsDao
 
-    companion object{
-        private var databaseInstance: InjectionsDatabase ?= null
-
-        fun getInstance(context: Context): InjectionsDatabase{
-            return databaseInstance ?: synchronized(this){
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    InjectionsDatabase::class.java,
-                    "injection_database"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
-
-                databaseInstance = instance
-                instance
-            }
-        }
-    }
 }
