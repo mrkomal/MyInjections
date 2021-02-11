2b42654a5c0417d416bb097c020e2250a6c97ae8
mrkomal <gry2@vp.pl>
2021-02-11 15:15:27 +0100

    Working LiveData test for getting list from repo

--- app/src/test/java/com/example/myinjections/viewmodel/InjectionsViewModelTest.kt
+++ app/src/test/java/com/example/myinjections/viewmodel/InjectionsViewModelTest.kt
@@ -1,36 +1,18 @@
 package com.example.myinjections.viewmodel
 
-import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.asLiveData
-import androidx.test.core.app.ApplicationProvider
-import com.example.myinjections.InjectionsApplication
 import com.example.myinjections.MainCoroutineRule
 import com.example.myinjections.getOrAwaitValueTest
 import com.example.myinjections.repository.FakeInjectionsRepositoryImpl
 import com.example.myinjections.repository.InjectionsRepository
-import com.example.myinjections.room.model.InjectionInfo
-import com.google.common.truth.Truth.assertThat
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.compat.ViewModelCompat.getViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.module
+import org.koin.dsl.koinApplication
 import org.koin.test.KoinTest
-import org.koin.test.get
-import org.koin.test.inject
-import org.mockito.Mockito.mock
 
 class InjectionsViewModelTest : KoinTest {
     @get:Rule
@@ -40,42 +22,21 @@ class InjectionsViewModelTest : KoinTest {
     @get:Rule
     val mainCoroutineRule = MainCoroutineRule()
 
-//    private val injectionsViewModelTestModule = module {
-//        single { FakeInjectionsRepositoryImpl() }
-//        viewModel { InjectionsViewModel(get()) }
-//    }
-
-   // private val fakeInjectionsViewModel: InjectionsViewModel = get()
-   //private val fakeInjectionsViewModel: InjectionsViewModel by inject()
-
-    private lateinit var repository: InjectionsRepository
-    private lateinit var viewModel: InjectionsViewModel
+    private lateinit var testRepository: InjectionsRepository
+    private lateinit var testViewModel: InjectionsViewModel
 
     @Before
     fun setup() {
-//        startKoin{
-//            androidContext(mock(Context::class.java))
-//            modules(injectionsViewModelTestModule)
-//        }
-       // val viewModel: InjectionsViewModel = getViewModel()
-        repository = FakeInjectionsRepositoryImpl()
-        viewModel = InjectionsViewModel(repository)
+        testRepository = FakeInjectionsRepositoryImpl()
+        testViewModel = InjectionsViewModel(testRepository)
     }
 
     @After
-    fun tearDown() {
-//        stopKoin()
-    }
+    fun tearDown() { }
 
     @Test
     fun getAllInjectionInfoWorksCorrectly_returns_true() {
-        //val fakeInjectionsViewModel: InjectionsViewModel = get()
-//        val repository: InjectionsRepository by inject()
-        //val fakeInjectionsViewModel: InjectionsViewModel by inject()
-        //val a = repository.getAllInjectionInfo()
-        //assertThat(a == listOf<InjectionInfo>())
-//        val viewModel = InjectionsViewModel(repository)
-        val allInfo = viewModel.resultInjectionInformation.getOrAwaitValueTest()
+        val allInfo = testViewModel.resultInjectionInformation.getOrAwaitValueTest()
         assertEquals(3, allInfo.count())
     }
 }
