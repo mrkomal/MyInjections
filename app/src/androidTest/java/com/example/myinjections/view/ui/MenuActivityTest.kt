eeac87f20cc596cf4c04c9913091d3099ddd571a
mrkomal <gry2@vp.pl>
2021-02-16 00:18:47 +0100

    Instrumental tests for MenuActivity
    Checking if clicking buttons results in launching desired activity/

---
+++ app/src/androidTest/java/com/example/myinjections/view/ui/MenuActivityTest.kt
@@ -0,0 +1,53 @@
+package com.example.myinjections.view.ui
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.activity.result.ActivityResult
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.myinjections.R
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertTrue
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+
+@RunWith(AndroidJUnit4::class)
+class MenuActivityTest : KoinTest {
+
+    @get:Rule
+    val activityRule: ActivityScenarioRule<MenuActivity>
+            = ActivityScenarioRule(MenuActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
+    @Test
+    fun clickButton_launchesAddInjectionActivity_returnsTrue() {
+        onView(withId(R.id.add_injection_button)).perform(click())
+        intended(hasComponent(AddInjectionActivity::class.java.name))
+    }
+
+    @Test
+    fun clickButton_launchesDisplayInjectionsActivity_returnsTrue() {
+        onView(withId(R.id.display_injections_button)).perform(click())
+        intended(hasComponent(DisplayInjectionActivity::class.java.name))
+    }
+}
