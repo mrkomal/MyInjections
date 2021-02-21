00147c6260d797f397dab06bfb0d6aba39f1818f
mrkomal <gry2@vp.pl>
2021-02-22 00:33:20 +0100

    MenuActivity tests
    Testing the performance of add and display buttons

--- app/src/androidTest/java/com/example/myinjections/view/ui/MenuActivityTest.kt
+++ app/src/androidTest/java/com/example/myinjections/view/ui/MenuActivityTest.kt
@@ -1,9 +1,5 @@
 package com.example.myinjections.view.ui
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
-import androidx.activity.result.ActivityResult
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents
@@ -13,8 +9,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.myinjections.R
-import junit.framework.Assert.assertNotNull
-import junit.framework.Assert.assertTrue
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
