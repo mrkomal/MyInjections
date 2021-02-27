ce4b2db3135baca0f46bc7d1f39548a695f10b7b
mrkomal <gry2@vp.pl>
2021-02-27 17:24:59 +0100

    Repository with getAllLinks funtion
    Get data with use of defined service

--- app/src/main/java/com/example/myinjections/repository/usefullinks/UsefulLinksRepositoryImpl.kt
+++ app/src/main/java/com/example/myinjections/repository/usefullinks/UsefulLinksRepositoryImpl.kt
@@ -3,9 +3,11 @@ package com.example.myinjections.repository.usefullinks
 import com.example.myinjections.network.model.UsefulLink
 import com.example.myinjections.network.service.UsefulLinksService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class UsefulLinksRepositoryImpl(private val usefulLinksService: UsefulLinksService): UsefulLinksRepository {
-    override suspend fun getAllLinks(): Flow<List<UsefulLink>> {
-        return usefulLinksService.getLinks()
+class UsefulLinksRepositoryImpl(private val usefulLinksService: UsefulLinksService) : UsefulLinksRepository {
+
+    override suspend fun getAllLinks(): Flow<List<UsefulLink>> = flow {
+        emit(usefulLinksService.getLinks())
     }
 }
