3f76e2b7503ee3b6814f4de6378b75e144900478
mrkomal <gry2@vp.pl>
2021-02-27 17:23:16 +0100

    Get all files from DB function

--- app/src/main/java/com/example/myinjections/network/service/UsefulLinksService.kt
+++ app/src/main/java/com/example/myinjections/network/service/UsefulLinksService.kt
@@ -1,10 +1,9 @@
 package com.example.myinjections.network.service
 
 import com.example.myinjections.network.model.UsefulLink
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 
 interface UsefulLinksService {
-    @GET("/posts")
-    suspend fun getLinks(): Flow<List<UsefulLink>>
+    @GET("posts")
+    suspend fun getLinks(): List<UsefulLink>
 }
