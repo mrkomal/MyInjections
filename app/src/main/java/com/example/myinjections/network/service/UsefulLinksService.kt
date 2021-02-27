6b494741a6659a0d95de2018e8bc2ccc5f581ed2
mrkomal <gry2@vp.pl>
2021-02-27 01:18:49 +0100

    Definig get service

--- app/src/main/java/com/example/myinjections/network/service/UsefulLinksService.kt
+++ app/src/main/java/com/example/myinjections/network/service/UsefulLinksService.kt
@@ -1,5 +1,10 @@
 package com.example.myinjections.network.service
 
-interface UsefulLinksService {
+import com.example.myinjections.network.model.UsefulLink
+import kotlinx.coroutines.flow.Flow
+import retrofit2.http.GET
 
+interface UsefulLinksService {
+    @GET("/posts")
+    suspend fun getLinks(): Flow<List<UsefulLink>>
 }
