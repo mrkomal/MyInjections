b507262a07ff2bc78d1e1e6ec8f05461a7dc1aaa
mrkomal <gry2@vp.pl>
2021-02-27 17:27:46 +0100

    Testing getAll files function in service

---
+++ app/src/test/java/com/example/myinjections/network/UsefulLinksServiceTest.kt
@@ -0,0 +1,47 @@
+package com.example.myinjections.network
+
+import com.example.myinjections.modules.networkModule
+import com.example.myinjections.network.model.UsefulLink
+import com.example.myinjections.network.service.UsefulLinksService
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+
+class UsefulLinksServiceTest : KoinTest {
+
+    private val service: UsefulLinksService by inject()
+    
+    // Example JSON that is inside database.
+    private val usefulLink: UsefulLink = UsefulLink(id=1, title="Post 1")
+
+    @Before
+    fun setUp() {
+        /*
+        Application uses testing api so there is no need to create new api for testing.
+        That is why network module with Retrofit definition will be used.
+         */
+        startKoin {
+            modules(networkModule)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun test(): Unit = runBlocking {
+        val valsFromApi = service.getLinks()
+        assertTrue("Api returns nothing or does not contain searched element.",
+            valsFromApi.contains(usefulLink)
+        )
+    }
+
+}
