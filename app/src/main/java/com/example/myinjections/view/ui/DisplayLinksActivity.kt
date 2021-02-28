c5e9232195f10845b125eac60466c1e2e4448c9a
mrkomal <gry2@vp.pl>
2021-02-28 16:43:55 +0100

    Setting toolbar for activity

--- app/src/main/java/com/example/myinjections/view/ui/DisplayLinksActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/DisplayLinksActivity.kt
@@ -8,5 +8,13 @@ class DisplayLinksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_links)
+
+        //Setting toolbar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.display_links_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
