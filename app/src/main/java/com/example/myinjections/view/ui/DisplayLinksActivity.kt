574bbbcbb965e89de7a4dd13b935b1dfe5d66764
mrkomal <gry2@vp.pl>
2021-03-15 21:51:16 +0100

    Coroutine that takes care for starting and finishing progressbar

--- app/src/main/java/com/example/myinjections/view/ui/DisplayLinksActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/DisplayLinksActivity.kt
@@ -2,12 +2,23 @@ package com.example.myinjections.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.annotation.MainThread
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.myinjections.R
 import com.example.myinjections.view.adapters.UsefulLinksViewPagerAdapter
 import com.example.myinjections.viewmodel.UsefulLinksViewModel
+import kotlinx.android.synthetic.main.activity_display_links.*
+import kotlinx.android.synthetic.main.usefullinks_viewpager_item.*
+import kotlinx.coroutines.*
 import org.koin.androidx.viewmodel.ext.android.getViewModel
+import java.util.*
+import kotlin.concurrent.schedule
 
 class DisplayLinksActivity : AppCompatActivity() {
 
@@ -28,6 +39,20 @@ class DisplayLinksActivity : AppCompatActivity() {
 
         // ViewModel
         usefulLinksViewModel = getViewModel()
+
+        // Display progress bar until data is loaded
+        GlobalScope.launch {
+            // display progress bar
+            progress_bar.isVisible = true
+
+            // do nothing until page title appears
+            while(page_title_textview == null){
+                // page title is still null
+            }
+
+            // if first title appears then hide progress bar
+            progress_bar.isVisible = false
+        }
 
         // RecyclerView
         initializeRecyclerView()
