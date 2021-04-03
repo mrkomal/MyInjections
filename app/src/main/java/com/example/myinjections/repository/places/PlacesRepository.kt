4d2d30caddf98b5a5f4698b35b7a41e8e97f9a78
mrkomal <gry2@vp.pl>
2021-04-03 23:28:21 +0200

    Updating places repository

--- app/src/main/java/com/example/myinjections/repository/places/PlacesRepository.kt
+++ app/src/main/java/com/example/myinjections/repository/places/PlacesRepository.kt
@@ -1,7 +1,10 @@
 package com.example.myinjections.repository.places
 
 import com.example.myinjections.network.model.Place
+import kotlinx.coroutines.flow.Flow
 
 interface PlacesRepository {
-    suspend fun getAllPlaces(): List<Place>
+    suspend fun getAllPlaces(): Flow<List<Place>>
+
+    suspend fun getAllPharmacies(): Flow<List<Place>>
 }
--- app/src/main/java/com/example/myinjections/repository/places/PlacesRepositoryImpl.kt
+++ app/src/main/java/com/example/myinjections/repository/places/PlacesRepositoryImpl.kt
@@ -2,10 +2,18 @@ package com.example.myinjections.repository.places
 
 import com.example.myinjections.network.model.Place
 import com.example.myinjections.network.service.PlaceService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class PlacesRepositoryImpl(private val placeService: PlaceService): PlacesRepository {
-    override suspend fun getAllPlaces(): List<Place> {
-        return placeService.getAllPlaces()
+
+    override suspend fun getAllPlaces(): Flow<List<Place>> = flow {
+        emit(placeService.getAllPlaces())
+    }
+
+    override suspend fun getAllPharmacies(): Flow<List<Place>> = flow {
+        val placeType = "pharmacy"
+        emit(placeService.getAllPlacesOfSpecifiedType(placeType))
     }
 
 }
