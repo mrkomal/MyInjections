26e0ea4c98c686065ca0454aa1425e7d95840459
mrkomal <gry2@vp.pl>
2021-04-06 17:05:59 +0200

    Updating function to get all places, adding function to get only pharmacies

--- app/src/main/java/com/example/myinjections/repository/places/PlacesRepository.kt
+++ app/src/main/java/com/example/myinjections/repository/places/PlacesRepository.kt
@@ -4,7 +4,7 @@ import com.example.myinjections.network.model.Place
 import kotlinx.coroutines.flow.Flow
 
 interface PlacesRepository {
-    suspend fun getAllPlaces(): Flow<List<Place>>
+    fun getAllPlaces(): Flow<List<Place>>
 
     suspend fun getAllPharmacies(): Flow<List<Place>>
 }
--- app/src/main/java/com/example/myinjections/repository/places/PlacesRepositoryImpl.kt
+++ app/src/main/java/com/example/myinjections/repository/places/PlacesRepositoryImpl.kt
@@ -2,14 +2,16 @@ package com.example.myinjections.repository.places
 
 import com.example.myinjections.network.model.Place
 import com.example.myinjections.network.service.PlaceService
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class PlacesRepositoryImpl(private val placeService: PlaceService): PlacesRepository {
 
-    override suspend fun getAllPlaces(): Flow<List<Place>> = flow {
+    override fun getAllPlaces(): Flow<List<Place>> = flow {
         emit(placeService.getAllPlaces())
-    }
+    }.flowOn(Dispatchers.IO)
 
     override suspend fun getAllPharmacies(): Flow<List<Place>> = flow {
         val placeType = "pharmacy"
