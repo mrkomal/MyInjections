1c36547aca554af632e65c5ee4d85fb0870b8ed7
mrkomal <gry2@vp.pl>
2021-04-19 17:34:20 +0200

    Injection amount activity and layout

---
+++ app/src/main/java/com/example/myinjections/view/ui/InjectionAmountActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myinjections.view.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myinjections.R
+
+class InjectionAmountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_injection_amount)
+    }
+
+
+}
