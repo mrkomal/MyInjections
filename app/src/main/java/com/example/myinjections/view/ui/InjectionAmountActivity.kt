afb91783dbe64bcdcaa38ad0e82f54730a00278c
mrkomal <gry2@vp.pl>
2021-04-27 10:51:20 +0200

    Raw version of images
    Fixing problem with displaying imageview. Simple listener to change picture's state

--- app/src/main/java/com/example/myinjections/view/ui/InjectionAmountActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/InjectionAmountActivity.kt
@@ -1,18 +1,19 @@
 package com.example.myinjections.view.ui
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myinjections.R
 
 class InjectionAmountActivity : AppCompatActivity() {
 
-    private val TAG = InjectionAmountActivity::class.java.simpleName
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_injection_amount)
+
+        val a = findViewById<ImageView>(R.id.injection_picture)
+        a.setOnClickListener {
+            a.setImageResource(R.drawable.pic_injection_state_1)
+        }
     }
-
-
 }
