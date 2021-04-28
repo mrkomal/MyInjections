88faba87d05bf86a1389d66a4de685d48b44ad0f
mrkomal <gry2@vp.pl>
2021-04-28 19:26:33 +0200

    Setting toolbar for injection amount and basic appearance

--- app/src/main/java/com/example/myinjections/view/ui/InjectionAmountActivity.kt
+++ app/src/main/java/com/example/myinjections/view/ui/InjectionAmountActivity.kt
@@ -11,6 +11,14 @@ class InjectionAmountActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_injection_amount)
 
+        // Setting toolbar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.display_links_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
         val a = findViewById<ImageView>(R.id.injection_picture)
         a.setOnClickListener {
             a.setImageResource(R.drawable.pic_injection_state_1)
